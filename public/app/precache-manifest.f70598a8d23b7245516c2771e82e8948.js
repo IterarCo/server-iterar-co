fd37facbaf4057907703dfa91a096ebf6cdb9083
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-10-11 14:25:30 -0500

    Upodate

---
+++ public/app/precache-manifest.f70598a8d23b7245516c2771e82e8948.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b37eb8a5dbdea7e01886",
+    "url": "/app/js/chunk-vendors~d2305125.718e2e4c.js"
+  },
+  {
+    "revision": "8ded952b9ab719e090bf3ed987f944b8",
+    "url": "/app/noscript/noscript.css"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/app/robots.txt"
+  },
+  {
+    "revision": "07d773930c4fc8516d79",
+    "url": "/app/js/chunk-vendors~fdc6512a.eeda7753.js"
+  },
+  {
+    "revision": "158bae353308d27d17fb",
+    "url": "/app/js/chunk-vendors~253ae210.a584fca4.js"
+  },
+  {
+    "revision": "b6f6290902251daa8555",
+    "url": "/app/js/chunk-vendors~1c3a2c3f.9640ce24.js"
+  },
+  {
+    "revision": "675bec2bd7d5061162af",
+    "url": "/app/js/chunk-vendors~0605657e.af5cec8b.js"
+  },
+  {
+    "revision": "7288d0786870e0ec74a0",
+    "url": "/app/js/app~d0ae3f07.1381f788.js"
+  },
+  {
+    "revision": "392067c077970761dea1",
+    "url": "/app/js/about~78946e6e.51f2174a.js"
+  },
+  {
+    "revision": "efe3331d4069aed6316efe6a8a6e2db9",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/app/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7288d0786870e0ec74a0",
+    "url": "/app/css/app~d0ae3f07.bbf12bb2.css"
+  }
+];
